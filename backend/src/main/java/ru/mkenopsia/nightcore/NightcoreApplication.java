3a7c7651ff379da637ad18e03a968094940a6666
mkenopsia <gxkurro@gmail.com>
2026-06-07 21:44:31 +0700

    add ci/cd with devsecops tools

--- backend/src/main/java/ru/mkenopsia/nightcore/NightcoreApplication.java
+++ backend/src/main/java/ru/mkenopsia/nightcore/NightcoreApplication.java
@@ -2,7 +2,9 @@ package ru.mkenopsia.nightcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties
 @SpringBootApplication
 public class NightcoreApplication {
 
